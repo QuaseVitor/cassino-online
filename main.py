fdfe6be708785c21785feb23ea676c6be03e0320
QuaseVitor <Jvitorcont@gmail.com>
2024-08-22 21:58:34 -0300

    Update com versão de jogadas automatizadas para teste de taxa de vitorias + adição do markdown.md

--- main.py
+++ main.py
@@ -1,39 +1,68 @@
 from random import randint
 import os
-saldo = int(input("Insira quanto quer jogar"))
+
+saldo = 1000
+
 multi = 0
 balan = 0
+jogadas = 0
 
-while True:   
+#Experimental
+win = 0
+lose = 0
+
+while jogadas != 100:   
     print("Seu saldo atual é de ", saldo)
-    valor = int(input("Quanto quer gastar?  R$  "))
-     
+    #valor = int(input("Quanto quer gastar?  R$  "))
+    valor = randint(5,21)
     os.system("cls")
+    print(valor)
+    print("Balanço atual: ",balan)
      
-    if valor <= 20:
-        if balan >= 4:
-            sorte = 1
-            multi = 1.3
-        else:
-            sorte = randint(0,2)
-            multi = 1.5
-    elif valor <= 50:
-        if balan >= 10:
-            sorte = 1
-            multi = 1.2
-        else:
-            sorte = randint(0,4)
-            multi = 1.25
-    else:
-        if balan >= 15:
-            sorte = 1
-            multi = 1.1
-        else:
-            sorte = randint(0,9) 
-            multi = 1.1
-            
+# Balanço de 3 primeiras jogadas    
+     
+    if jogadas==0 and valor<= 10:
+         sorte = 1
+         multi = 2.5
+         
+    elif jogadas == 1 and valor<= 10:
+        sorte = randint(0,1) 
+        multi = 1.5
+        
+    elif jogadas == 2 and valor >= 10:
+        sorte =  randint(0,9)
+        multi = 1.4
     
-    x = 0
+    else: 
+             
+#  Balanceamento de jogadas não manipuladas             
+             
+        if valor <= 20:
+            if balan <= 3:
+                sorte = randint(0,2)
+                multi = 1.3
+            else:
+                sorte = randint(0,3)
+                multi = 1.5
+        elif valor <= 50:
+            if balan >= 10:
+                sorte = 1
+                multi = 1.2
+            else:
+                sorte = randint(0,4)
+                multi = 1.25
+        elif valor <= 100:
+            sorte = randint (0,9)
+            multi = 1.2
+            
+        elif valor <= 500:   
+            sorte = randint(0,25) 
+            multi = 1.5   
+        else:
+            sorte = randint(0,30) 
+            multi = 1.25
+            
+               
     teste = randint (0,5)
     teste1 = randint (0,5)
     teste2 = randint (0,5)
@@ -49,16 +78,27 @@ while True:
     if teste == teste1 and teste == teste2 and teste1 == teste2:
         
         valor = valor*multi
-        print("Parabens voce ganhou R$",valor)
+        print("Parabens voce ganhou R$", valor)
+        balan = 0
         
+        
+    # experimento
+        win += 1
         
     else:
-        print("kkkkkkksefudeu")
+        print("Poxa, não foi dessa vez. vamos outra?")
         balan += 1  
         valor = valor*-1
-     
+
+    # experimento
+        lose += 1
  
     saldo += valor   
+    
+    jogadas += 1
+    print( "Vezes jogadas: ",jogadas)
+    
+    print("Vitorias: ", win, "derrotas ", lose)
     
 #Quando o saldo zerar    
     
---
+++ markdown.md
@@ -0,0 +1,3 @@
+Update especifico para testes 
+
+Esta versão serve apenas para testar o balanceamento da taxa de vitoria do jogo. Deverá ser desconsiderada futuramente.
